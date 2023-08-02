098d45349463404d1262179b4892aedbbca656c9
Oleg Chumin <tschumin.oleg@gmail.com>
2023-08-02 08:12:22 +0300

    CRUDE code

---
+++ src/main/java/com/example/springkafkamicroservices/SpringKafkaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkafkamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springkafkamicroservices/SpringKafkaMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springkafkamicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
